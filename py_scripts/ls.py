0584579df06ec787dfd013b62ad68806d162a95f
vladimir <vladimir.dumanovskii@wayray.com>
2017-03-30 10:32:18 +0300

    Add 'ls' command.

---
+++ py_scripts/ls.py
@@ -0,0 +1,20 @@
+#num_args = 9
+# ls
+
+from sys import platform
+import sys
+import os
+
+cmd_args = ''
+if len(sys.argv) > 1:
+    for arg in sys.argv[:1]:
+        cmd_args = arg + " "
+cmd = ''
+if platform == "linux" or platform == "linux2":
+    cmd = 'ls ' + cmd_args
+elif platform == "darwin":
+    cmd = 'ls ' + cmd_args
+elif platform == "win32":
+    os.system("powershell set-executionpolicy remotesigned")
+    cmd = 'powershell ls ' + cmd_args
+os.system(cmd)
